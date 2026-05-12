2e4ceadaa2c7295091e9c6334de5ca3203db5679
BelindaMtabane <164875190+BelindaMtabane@users.noreply.github.com>
2026-05-12 11:22:22 +0200

    First commit
    The  player moves, jumps and slides properly without hesitation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Declare variables
+    private CharacterController controller;
+    public float sidewaySpeed = 5f;
+    public float forwardSpeed = 10f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
 
-    // Update is called once per frame
+    //Ground variables
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    //Vector variable
+    private float velocity;
+    public LayerMask Ground;
+
+    private void Start()
+    {
+        //Initialize character controller
+        controller = GetComponent<CharacterController>();
+    }
     void Update()
     {
+        //Lock the cursor to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //Check if player is grounded
+
+        groundMask = Ground;
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity < 0)
+        {
+            velocity = -2f;
+        }
+        Debug.DrawRay(groundCheck.position, Vector3.down * groundDistance, Color.red);
+        Debug.Log(controller.isGrounded);
+        //Initialize horixontal movement
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //Create movement vector
+        Vector3 move = new Vector3(horizontal * sidewaySpeed, 0, forwardSpeed);
         
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //Apply Gravity
+        velocity += gravity * Time.deltaTime;
+        //Initialize gravity to movement
+        move.y = velocity;
+        //Move the player
+        controller.Move(move * Time.deltaTime);
     }
 }
